73d2e1a5e023783db5a2f71947de490580fa4fc6
Anseltis <the-ansi@yandex.ru>
2017-12-20 16:22:40 +0300

    Added react

---
+++ src/Web/components/todo-layout/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { TodoLayoutProps } from './prop';
+
+export const TodoLayout: React.StatelessComponent<TodoLayoutProps> =
+    (props: TodoLayoutProps): JSX.Element => {
+        return <div >Hello world!!!</div>;
+    };
---
+++ src/Web/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { TodoLayout } from './components/todo-layout';
+
+ReactDOM.render(
+    <TodoLayout/>,
+    document.getElementById('root'));
